0916351776174bc4b02e7bb396fbd583c8d84ae2
KatK-32 <159088831+KatK-32@users.noreply.github.com>
2026-03-14 12:01:29 -0500

    Add files via upload

---
+++ Day 17/quiz-game-start/main.py
@@ -0,0 +1,24 @@
+from question_model import Question
+from data import question_data_1, question_data_2
+from quiz_brain import QuizBrain
+
+question_bank = []
+# for question in question_data_1:
+#     question_text = question["text"]
+#     question_answer = question["answer"]
+#     new_question = Question(question_text, question_answer)
+#     question_bank.append(new_question)
+
+for question in question_data_2:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_question():
+    quiz.next_question()
+
+print("You've completed the quiz!")
+print(f"Your final score is: {quiz.score}/{quiz.question_number}")
---
+++ Day 8/Functions with Inputs/task.py
@@ -0,0 +1,13 @@
+# Simple Function that packages code into a named block
+def greet():
+    print("Hello Katherine.")
+    print("How do you do Katherine Krueger?")
+    print("Isn't the weather nice?")
+
+greet()
+
+def greet_with_name(name):
+    print(f"Hello {name}.")
+    print(f"How do you do {name}?")
+
+greet_with_name("Caitlyn")
